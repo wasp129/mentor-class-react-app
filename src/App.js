d6b17306e50bece551570fad19bd86c3f289cdbe
Andreas Schultz <andreas.schultz@mediacom.dk>
2018-03-01 11:52:21 +0100

    Added semantic UI to package.json

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 
+const HeaderElements = props => <p>{props.children}</p>;
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="header">
+        <HeaderElements>Menu item 1</HeaderElements>
+        <HeaderElements>Menu item 2</HeaderElements>
+        <HeaderElements>Menu item 3</HeaderElements>
       </div>
     );
   }
