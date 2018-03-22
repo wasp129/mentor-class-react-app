12c6316761345e5f14adfaa054f1178f18eb8f01
Andreas Schultz <andreas.schultz@mediacom.dk>
2018-03-22 14:12:53 +0100

    yo recipes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Logo from "./react.svg";
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Header title="React Guide"/>
+        <Body/>
       </div>
     );
   }
@@ -13,12 +15,44 @@ class App extends Component {
 class Header extends Component {
   render() {
     return (
-      <div>
-        <p></p>
+      <div className="header">
+        <p className="title">{this.props.title}</p>
+        <img src={Logo} alt="Alt"/>
       </div>
     );
   }
 }
+
+class Body extends Component {
+  render() {
+    return(
+      <div className="body">
+        <Overview/>
+      </div>
+    )
+  }
+}
+
+class Overview extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="headline">WELCOME TO THE FIRST DAY OF THE REST OF YOUR LIFE.</h1>
+        <p>This is an introduction to React. We're going to have fun, learn some stuff, make money and get bitches.</p>
+      </div>
+    )
+  }
+}
+
+//ANDREAS START
+
+// ANDREAS STOP
+
+
+// Svilena START
+
+
+// Svilena STOP
 
 export default App;
 
