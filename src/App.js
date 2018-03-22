2f9ef49fe034e90221b05d6406b6ee05940ee9d3
Andreas Schultz <andreas.schultz@mediacom.dk>
2018-03-22 11:14:09 +0100

    Changed a few things

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react'
-import { Card, Icon, Image } from 'semantic-ui-react'
-
-// const HeaderElements = props => <p>{props.children}</p>;
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header as='h1'>React 101</Header>
-        <Card>
-          <Image src='/assets/images/avatar/large/matthew.png' />
-          <Card.Content>
-            <Card.Header>
-              Matthew
-            </Card.Header>
-            <Card.Meta>
-              <span className='date'>
-                Joined in 2015
-              </span>
-            </Card.Meta>
-          <Card.Description>
-            Matthew is a musician living in Nashville.
-        </Card.Description>
-      </Card.Content>
-  </Card>
+        
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <p></p>
       </div>
     );
   }
