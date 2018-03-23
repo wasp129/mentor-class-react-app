08d8779b27d157c61eb21e3b11dfe7e555b1f66f
petyakazakova <pk.kazakova@gmail.com>
2018-03-23 11:34:53 +0100

    f

--- src/App.js
+++ src/App.js
@@ -1,24 +1,111 @@
 import React, { Component } from 'react';
+import logo from './logo.png';
+
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Header title="React Guide" />
+        <Body/>
       </div>
     );
   }
 }
 
+//header element
 class Header extends Component {
   render() {
     return (
-      <div>
-        <p></p>
+      <div className="header">
+        <img className="logo" src={logo} alt="React logo" />
+        <p className="title">{this.props.title}</p>
       </div>
     );
   }
 }
 
+class Body extends Component {
+  render() {
+    return (
+      <div className="body">
+        <Overview/> 
+      </div>
+    )
+  }
+}
+
+class Overview extends Component {
+  render() {
+    return (
+      <div className="overview">
+        <h1 className="headline">THIS IS A HEADLINE</h1>
+        <p>Introduction to React.</p>
+      </div>
+    )
+  }
+}
+
+
 export default App;
+
+
+/*
+class Sidebar extends Component {
+  render() {
+    return (
+    
+    )
+  }
+}
+
+class SidebarSteps extends Component { 
+  render() {
+    return(
+
+    )
+  }
+}
+
+//FOOTER COMPONENT
+
+//arrays with objects
+const prerequisits = [
+  //collection of elements
+  {
+    step: "Overview",
+    step: "Node.js",
+    step: "Babel",
+    step: "Git",
+    step: "JSX/ES6",
+    step: "Semantic UI"
+  }
+]
+
+const development = [
+  {
+    step: "Hello world",
+    step: "Complex" 
+  }
+]
+
+const action = [
+  {
+    step: "Build a landingpage",
+    step: "Build an article page",
+    step: "Optional: To-do list with DB"
+  }
+]
+
+const deployment = [
+  {
+    step: "Azure"
+  }
+]
+
+console.log(prerequisits, development, action, deployment);
+
+*/
+
 
