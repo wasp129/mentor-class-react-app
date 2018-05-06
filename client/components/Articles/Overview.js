edc85b232e1f66d76410ef6e849bae72388c6b87
Jane Doe <svilena_koleva@yahoo.com>
2018-05-06 17:57:14 +0200

    Kinda did the overview modal, still problem with the img tho..

--- client/components/Articles/Overview.js
+++ client/components/Articles/Overview.js
@@ -2,33 +2,33 @@ import React, { Component } from 'react';
 import { Button, Header, Icon, Image, Modal } from 'semantic-ui-react'
 
 const Overview = () => (
-  <Modal trigger={<Button>Long Modal</Button>}>
-    <Modal.Header>Profile Picture</Modal.Header>
+  <Modal trigger={<Button>Open Overview</Button>} closeIcon>
+    <Modal.Header>Overview</Modal.Header>
     <Modal.Content image>
-      <Image wrapped size='medium' src='/assets/images/wireframe/image.png' />
+      <Image wrapped size='medium' src='images/ov-1.png' />
       <Modal.Description>
-        <Header>Modal Header</Header>
+        <Header><h2>Chapter 1</h2></Header>
         <p>
-        	This is an example of expanded content that will cause the modal's dimmer to scroll.
-        	This is an example of expanded content that will cause the modal's dimmer to scroll.
-        	This is an example of expanded content that will cause the modal's dimmer to scroll.
-        	This is an example of expanded content that will cause the modal's dimmer to scroll.
-        	This is an example of expanded content that will cause the modal's dimmer to scroll.
-        	This is an example of expanded content that will cause the modal's dimmer to scroll.
-        	This is an example of expanded content that will cause the modal's dimmer to scroll.
-        	This is an example of expanded content that will cause the modal's dimmer to scroll.
-        	This is an example of expanded content that will cause the modal's dimmer to scroll.
-        	This is an example of expanded content that will cause the modal's dimmer to scroll.
-        	This is an example of expanded content that will cause the modal's dimmer to scroll.
-        	This is an example of expanded content that will cause the modal's dimmer to scroll.
-        	This is an example of expanded content that will cause the modal's dimmer to scroll.
-        	This is an example of expanded content that will cause the modal's dimmer to scroll.
-        	This is an example of expanded content that will cause the modal's dimmer to scroll.
-        	This is an example of expanded content that will cause the modal's dimmer to scroll.
-        	This is an example of expanded content that will cause the modal's dimmer to scroll.
-        	This is an example of expanded content that will cause the modal's dimmer to scroll.
-        	This is an example of expanded content that will cause the modal's dimmer to scroll.
+            ReactJS is an open-source JavaScript library which is used for building user interfaces specifically for single page applications. React allows developers to create large web applications which can change data, without reloading the page. The main purpose of React is to be fast, scalable, and simple. It is used for handling the view layer (or user interfaces) of web applications. This corresponds to view in the MVC template. 
         </p>
+        <p>
+            At the heart of all React applications are components. Broadly speaking, to write React apps we write components that correspond to various interface elements. We then organize these components inside higher-level components which define the structure of our application. The components correspond to interface elements. React also allows us to create reusable UI components. 
+        </p>
+        <h3>Why React?</h3>
+        <dl> 
+        <p>These are some of the benefits that React offers:</p>
+          <dt><b>Simplicity</b></dt>
+          <dd>- ReactJS is simpler to grasp right away. The component-based approach, well-defined lifecycle, and use of just plain JavaScript make React very simple to learn. React uses a special syntax called JSX which allows you to mix HTML with JavaScript. This is not a requirement but JSX is much easier to use.</dd>
+          <br/>
+          <dt><b>Easy to learn</b></dt>
+          <dd>- Anyone with a basic previous knowledge in programming can easily understand React. For react you just need basic knowledge of CSS, HTML and JavaScript.</dd>
+          <br/>
+          <dt><b>Data Binding</b></dt>
+          <dd>- React uses one-way data binding and an application architecture called Flux controls the flow of data to components through one control point – the dispatcher. It's easier to debug self-contained components of large ReactJS apps.</dd>
+          <br/>
+          <dt><b>Testability</b></dt>
+          <dd>- ReactJS applications are super easy to test. React views can be treated as functions of the state, so we can manipulate with state we pass to the ReactJS view and take a look at the output and triggered actions, events, functions, etc.</dd>
+        </dl>
       </Modal.Description>
     </Modal.Content>
     <Modal.Actions>
