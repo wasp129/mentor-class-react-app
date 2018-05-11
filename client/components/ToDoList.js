1749b581fe71647b235e4917da36893b97ca2b2b
petyakazakova <pk.kazakova@gmail.com>
2018-05-11 12:01:38 +0200

    updated

--- client/components/ToDoList.js
+++ client/components/ToDoList.js
@@ -40,7 +40,7 @@ const onMakeDecision = () => {
 class ToDoList extends Component {
 	render() {
 		return (
-			<div>
+			<div className = "todo-container">
 				<h1>{app.title}</h1> 
 				{app.subtitle && <p>{app.subtitle}</p>} 
 				<p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
