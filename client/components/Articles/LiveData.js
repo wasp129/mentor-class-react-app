d49b0ab61377fc8e405d184c4181639b63fa8ab2
Jane Doe <svilena_koleva@yahoo.com>
2018-05-11 10:01:03 +0200

    fixed img on modal

---
+++ client/components/Articles/LiveData.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Button, Header, Icon, Image, Modal } from 'semantic-ui-react';
+import RealTimeData from './RealTimeData.js';
+
+const LiveData = () => (
+  <Modal trigger={<Button>Open Overview</Button>} closeIcon>
+    <Modal.Header>Overview</Modal.Header>
+    <Modal.Content image>
+      <Image className='modal-img' src='images/ov-1.png' />
+      <Modal.Description>
+        <Header><h2>Live Data</h2></Header>
+        <RealTimeData/>
+      </Modal.Description>
+    </Modal.Content>
+    
+  </Modal>
+)
+
+export default LiveData;
